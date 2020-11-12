d1e296a318a1e5212a0a3c92630de100383a1d7b
Zainab Mehdee <zainab.mehdee63@ed.cna.nl.ca>
2020-11-12 23:51:26 +0000

    Added binary conversion.

---
+++ main.py
@@ -0,0 +1,10 @@
+# Classes: Custom Data Types
+# Specialized Data Types
+# Modules that can be used from libraries.(Extensions, packages etc.
+# None = Nothing, 0 
+print(type(2+4))
+
+num_iq = 14
+print(bin(num_iq))
+
+print(int("0b1110", 2))
