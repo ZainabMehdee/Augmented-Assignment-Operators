504476bd3c345574ff0e5b7abc5397df628d3cc3
Zainab Mehdee <zainab.mehdee63@ed.cna.nl.ca>
2020-11-14 08:31:03 +0000

    Expressions vs. Statements

--- main.py
+++ main.py
@@ -8,3 +8,10 @@ num_iq = 14
 print(bin(num_iq))
 
 print(int("0b1110", 2))
+
+# What is an expression?
+# It's a piece of code that produces a value.
+
+user_height = 156
+user_age = user_height/5   # user_height/5 is an expression.
+# This whole line is a statement.
