06c7feeaa41a40ffe6c733aa02a8a5ce7fb9a177
Zainab Mehdee <zainab.mehdee63@ed.cna.nl.ca>
2020-11-14 08:35:38 +0000

    Augmented Assignment Operators

--- main.py
+++ main.py
@@ -15,3 +15,15 @@ print(int("0b1110", 2))
 user_height = 156
 user_age = user_height/5   # user_height/5 is an expression.
 # This whole line is a statement.
+
+
+# augmented assignment operator
+
+some_value = 4
+some_value += 3
+print(some_value)
+
+
+another_value = 34
+another_value -=14
+print(another_value)
